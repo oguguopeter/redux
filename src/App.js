88f20357f027bd038d6e8e25531bcf11cc694621
JONATHAN <oguguo.peter@gmail.com>
2023-06-09 10:23:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './counter/Counter'
+
+const App = () => {
+  return (
+    <div>
+
+      <Counter/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div>
+        <h1 > count </h1>
+        <button>count</button>
+        <button>increase</button>
+        <button>Reset</button>
+        <button>Decrease</button>
+    </div>
+  )
+}
+
+export default Counter
